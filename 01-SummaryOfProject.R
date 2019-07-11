e36887c1bae0213b0251889c09ba4136dd0081ac
Marcus Di Paula <marcusdipaula@gmail.com>
2019-07-11 14:53:48 -0300

    First commit

---
+++ 01-SummaryOfProject.R
@@ -0,0 +1,90 @@
+# ---
+# script: 01-SummaryOfProject
+# subject: Problem statement and comprehension of the context
+# date: 2019-07-10
+# author: Marcus Di Paula
+# github: github.com/marcusdipaula/
+# linkedin: linkedin.com/in/marcusdipaula/
+# ---
+
+
+#### Personal framework for a systematic approach ####
+
+
+#___________ FUNDAMENTALS ___________#
+#
+# 1. Problem statement and comprehension of the context
+#     - What am I trying to solve?
+#     - Who will benefit of/is asking for this solution?
+#     - What would be the ideal scenario for them?
+#     - How could I use the available data to help them achieve this scenario?
+#     - Why solve this problem? (purpose)
+#
+# 2. Looking for data: 
+#     - Identify entities (and its attributes) of the problem 
+#     - Collect data that represents entities
+#     - Which hypotheses could I state?
+#     - Explore the data (superficially) to understand it 
+#     - Could I use an algorithm to address the issue or solve it? Which one?
+
+
+
+#___________ DATA WRANGLING ___________#
+#
+# 3. Data preparation and Exploration (Feature Engineering oriented to the 4th phase)
+#     - Is my dataset tidy?
+#     - Is my dataset clean?
+#     - Which correlations exists between all variables and to the target?
+#     - There is any NA in my dataset? If so, how should I treat them? Which effects would it have?
+#     - Should I narrowing in on observations of interest? Which effects would it have?
+#     - Should I reduce my variables? Which effects would it have?
+#     - Should I create new variables that are functions of existing ones? Which effects would it have?
+#     - Should I binning variables? Which effects would it have?
+#     - should I convert variables (categorical = numerical / vv)? Which effects would it have?
+#     - Should I dummy coding categorical variables? Which effects would it have?
+#     - Should I standardize numerical variables? Which effects would it have?
+#     - Can I test my hypotheses?
+#     - 
+#
+# 4. Building and validating models
+#     - Train and test a ML model
+#     - Which performance metrics should I relly on?
+#     - Iteration
+
+
+
+#___________ DEPLOYING ___________#
+#
+# 5. Deploy
+#     - Data StoryTelling
+#     - How can I deploy the model to production?
+#     - Which strategies should I consider?
+#     - An overview of what should be considered: 
+# https://christophergs.github.io/machine%20learning/2019/03/17/how-to-deploy-machine-learning-models/
+
+
+
+#### End of framework ####
+
+
+
+# 1. Problem statement and comprehension of the context
+#     - What am I trying to solve?
+#       Predict/prevent potentially fraudulent clicks, characterized by lots of cliks
+#       but none installed apps.
+#
+#     - Who will benefit of/is asking for this solution?
+#       The Mobile Advertising Monitoring team, from the TalkingData company.
+#       The customers who paid for advertising through mobile marketing campaigns.
+#       
+#     - What would be the ideal scenario for them?
+#       To know, beforehand, who could (has the highest probability of) be a fraud cliker.
+#       To pay the right amount for (true) clicks.
+#       
+#     - How could I use the available data to help them achieve this scenario?
+#       By developing visualizations that would help understand what historical data is saying
+#       and by creating a predictive machine learning model that could classify each click.
+#       
+#     - Why solve this problem? (purpose)
+#       To help develop a data-driven culture and to show that crime does not pay (lol).
+#       
